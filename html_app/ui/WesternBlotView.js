bbed62ec93efcf62d3361ee2f506913ba56de4ce
Ivica Ceraj <ceraj@mit.edu>
2012-12-20 16:24:36 -0500

    Updates to setup

--- html_app/ui/WesternBlotView.js
+++ html_app/ui/WesternBlotView.js
@@ -14,19 +14,26 @@ scb.ui.WesternBlotView = function scb_ui_WesternBlotView(gstate) {
         var template = state.assignment.template;
         var grouped_rows = state.western_blot.lanes_list.grouped_list;
         var rows = [];
-        _.each(experiment.cell_treatment_list.list, function(e){
-            if(grouped_rows[e.id]){
-                _.each( grouped_rows[e.id], function(ee) {
-                   rows.push({
-                       kind:'existing',
-                       cell_treatment:e,
-                       lane:ee
-                   });
+        _.each(experiment.cell_treatment_list.list, function (e) {
+            if (grouped_rows[e.id]) {
+                _.each(grouped_rows[e.id], function (ee, index) {
+                    rows.push({
+                        kind:'existing',
+                        cell_treatment:e,
+                        lane:ee,
+                        display_sample: index != 0,
+                        index:index
+                    });
                 });
-            }else
-            {
+                rows.push({
+                    kind:'placeholder',
+                    display_sample:false,
+                    cell_treatment:e
+                });
+            } else {
                 rows.push({
                     row_type:'new',
+                    display_sample:true,
                     cell_treatment:e
                 })
             }
@@ -37,7 +44,7 @@ scb.ui.WesternBlotView = function scb_ui_WesternBlotView(gstate) {
             assignment:state.assignment,
             experiment:state.experiment,
             western_blot:state.western_blot,
-            rows: rows
+            rows:rows
         }));
         state.experiment.last_view = 'western_blot';
     }
