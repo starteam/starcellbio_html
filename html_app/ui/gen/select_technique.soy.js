eb2a9bb8b4c5857a77500658cabfd59ab24261c6
Ivica Ceraj <ceraj@mit.edu>
2013-01-15 16:54:11 -0500

    Adding styles

--- html_app/ui/gen/select_technique.soy.js
+++ html_app/ui/gen/select_technique.soy.js
@@ -20,13 +20,13 @@ scb_select_technique.display_details = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
   output.append('<div class=\'scb_s_select_technique_details_view\'>');
   scb_common.experiment_step({step: 4}, output);
-  output.append('<div class=\'scb_s_select_technique_western_blot\'>');
+  output.append('<div class=\'scb_s_select_technique_western_blot\'><h1>Available Western Blot:</h1><div class=\'scb_s_select_technique_western_blot_available\'>');
   var wList22 = opt_data.experiment.western_blot_list.list;
   var wListLen22 = wList22.length;
   for (var wIndex22 = 0; wIndex22 < wListLen22; wIndex22++) {
     var wData22 = wList22[wIndex22];
     output.append('<a class=\'scb_f_open_western_blot\' western_blot_id=\'', soy.$$escapeHtml(wData22.id), '\' href=\'#view=western_blot&experiment_id=', soy.$$escapeHtml(opt_data.experiment.id), '&assignment_id=', soy.$$escapeHtml(opt_data.assignment.id), '&western_blot_id=', soy.$$escapeHtml(wData22.id), '\'>', soy.$$escapeHtml(wData22.name), '<br>');
   }
-  output.append('<a class=\'scb_f_new_western_blot\' href=\'#view=western_blot&experiment_id=', soy.$$escapeHtml(opt_data.experiment.id), '&assignment_id=', soy.$$escapeHtml(opt_data.assignment.id), '\'>New Western Blot</a></div><a class="scb_f_open_experiment_setup_readonly" href="#view=experiment_run&assignment_id=', soy.$$escapeHtml(opt_data.assignment.id), '&experiment_id=', soy.$$escapeHtml(opt_data.experiment.id), '">Run Experiment</a><br/></div>');
+  output.append('</div><a class=\'scb_s_navigation_button scb_f_new_western_blot\' href=\'#view=western_blot&experiment_id=', soy.$$escapeHtml(opt_data.experiment.id), '&assignment_id=', soy.$$escapeHtml(opt_data.assignment.id), '\'>New Western Blot</a></div><a class="scb_f_open_experiment_setup_readonly" href="#view=experiment_run&assignment_id=', soy.$$escapeHtml(opt_data.assignment.id), '&experiment_id=', soy.$$escapeHtml(opt_data.experiment.id), '">Review set-up</a><br/></div>');
   return opt_sb ? '' : output.toString();
 };
