e32b165e91a5add5683e7de741248ea2b532915c
Ivica Ceraj <ceraj@mit.edu>
2013-03-14 17:21:03 -0400

    Added few more shapes

--- html_app/components/FACSModelFactory.js
+++ html_app/components/FACSModelFactory.js
@@ -1,26 +1,29 @@
 scb.components = scb.components || {};
 
 scb.components.FACSModelFactory = function scb_components_FACSModelFactory(model, template) {
-	var self = this;
+    var self = this;
 
-	if (scb.utils.isDefined(model.dna)) {
-		self.dna = function(state) {
+    if (scb.utils.isDefined(model.dna)) {
+        self.dna = function (state) {
             var t = template;
             var m = model.dna;
-            if(m.parser_simple)
-            {
+            if (m.parser_simple) {
                 var facs_lane = state.facs_lane;
                 var cell_treatment = facs_lane.cell_treatment;
                 var cell_line = cell_treatment.cell_line;
                 var drug_treatments = cell_treatment.treatment_list.list;
-                var shape = '' ;
+                var shape = '';
                 var facs_state = {
-                    cell_line:function(str) { return str == cell_treatment.cell_line},
-                    temperature:function(str) { return str == drug_treatments[0].temperature},
-                    drug_id: function(str) {
+                    cell_line: function (str) {
+                        return str == cell_treatment.cell_line
+                    },
+                    temperature: function (str) {
+                        return str == drug_treatments[0].temperature
+                    },
+                    drug_id: function (str) {
                         var any = false;
-                        _.each( drug_treatments , function(dt){
-                            _.each( dt.drug_list.list , function(drug){
+                        _.each(drug_treatments, function (dt) {
+                            _.each(dt.drug_list.list, function (drug) {
                                 any |= (drug.drug_id == str);
                             });
                         });
@@ -28,26 +31,22 @@ scb.components.FACSModelFactory = function scb_components_FACSModelFactory(model
                     }
                 }
 
-                _.each(m.parser_simple, function(rule){
+                _.each(m.parser_simple, function (rule) {
                     console.info(rule);
-                    if(rule.match.length == 0 )
-                    {
+                    if (rule.match.length == 0) {
                         shape = rule.shape;
                     }
-                    else
-                    {
+                    else {
                         var matches = true;
-                        _.each(rule.match, function(property){
-                            if( facs_state[property]) {
+                        _.each(rule.match, function (property) {
+                            if (facs_state[property]) {
                                 matches &= facs_state[property](rule[property]);
                             }
-                            else
-                            {
-                                console.info( "UNDEFINED PROPERTY: " + property);
+                            else {
+                                console.info("UNDEFINED PROPERTY: " + property);
                             }
                         });
-                        if( matches )
-                        {
+                        if (matches) {
                             shape = rule.shape;
                         }
                     }
@@ -56,72 +55,115 @@ scb.components.FACSModelFactory = function scb_components_FACSModelFactory(model
                 self.shape_to_data(state);
 
             }
-			// here we need to compute how this actually works
-			/* I think:
-				- Model will initiate random stuff when it starts
-				- Here we will move model depending on drugs
-				- And finish with collection time
-				- Visualizer will count from here and draw DNA curve
-			*/
-		}
-        self.shape_to_data = function(state)
-        {
+            // here we need to compute how this actually works
+            /* I think:
+             - Model will initiate random stuff when it starts
+             - Here we will move model depending on drugs
+             - And finish with collection time
+             - Visualizer will count from here and draw DNA curve
+             */
+        }
+        self.shape_to_data = function (state) {
             var shape = state.shape;
+
             function g0g1(x) {
-                return 2*Math.exp( -((x-1)*(x-1)) * 60 );
-            }
-            function near_zero(x) {
-                return 1/2*( x > 0 && x < 1 ? (.08 - x/50) : 0 );
-            }
-            function s(x) {
-                return .05 * (5*(x >.8 && x < 1?x -.8:0)+(x>1&x<2? (.8+(2-x)/5):0) /.6 +1.3*(x>2&x<2.3?(2.3-x)/.3:0));
-            }
-            function g2m(x) {
-                return 1/4*Math.exp(-((x-2)*(x-2)*30));
+                return 2 * Math.exp(-((x - 1) * (x - 1)) * 60);
             }
 
-            if( (''+shape).toLowerCase() == 'normal')
-            {
+            function near_zero(x) {
+                return 1 / 2 * ( x > 0 && x < 1 ? (.08 - x / 50) : 0 );
+            }
+
+            function s(x) {
+                return .05 * (5 * (x > .8 && x < 1 ? x - .8 : 0) + (x > 1 & x < 2 ? (.8 + (2 - x) / 5) : 0) / .6 + 1.3 * (x > 2 & x < 2.3 ? (2.3 - x) / .3 : 0));
+            }
+
+            function g2m(x) {
+                return 1 / 4 * Math.exp(-((x - 2) * (x - 2) * 30));
+            }
+
+            function normalize(data) {
+                var sum = 0;
+                _.each(data, function (s) {
+                    sum += s[1];
+                });
+                if (sum != 0) {
+                    _.each(data, function (s, index) {
+                        data[index][1] /= sum;
+                    });
+                }
+            }
+
+            if (('' + shape).toLowerCase() == 'normal') {
                 var data = [];
-                for( var x = 0 ; x < 3 ; x += .01)
-                {
-                    var y = g0g1(x)+g2m(x)+near_zero(x)+s(x);
-                    data.push([x,y]);
+                for (var x = 0; x < 3; x += .01) {
+                    var y = g0g1(x) + g2m(x) + near_zero(x) + s(x);
+                    data.push([x, y]);
 
                 }
+                normalize(data);
                 state.data = {
                     data: [
-                        {label:'DNA Content',data:data}
+                        {label: 'DNA Content', data: data}
                     ],
                     options: {
                         series: {
-                            lines: {show:true},
-                            points:{show:false}
+                            lines: {show: true},
+                            points: {show: false}
                         }
                     }
                 };
             }
-            if( (''+shape).toLowerCase() == 's-block')
-            {
+            if (('' + shape).toLowerCase() == 's-block') {
 
             }
-            if( (''+shape).toLowerCase() == 'g1-block')
-            {
+            if (('' + shape).toLowerCase() == 'g1-block') {
 
             }
-            if( (''+shape).toLowerCase() == 'g2-block')
-            {
+            if (('' + shape).toLowerCase() == 'g2-block') {
+                var data = [];
+                for (var x = 0; x < 3; x += .01) {
+                    var y = g2m(x);
+                    data.push([x, y]);
 
+                }
+                normalize(data);
+                state.data = {
+                    data: [
+                        {label: 'DNA Content', data: data}
+                    ],
+                    options: {
+                        series: {
+                            lines: {show: true},
+                            points: {show: false}
+                        }
+                    }
+                };
             }
-            if( (''+shape).toLowerCase() == 'alpha-block')
-            {
-
+            if (('' + shape).toLowerCase() == 'alpha-block') {
+                var data = [];
+                for (var x = 0; x < 3; x += .01) {
+                    var y = g0g1(x);
+                    data.push([x, y]);
+                }
+                normalize(data);
+                state.data = {
+                    data: [
+                        {label: 'DNA Content', data: data}
+                    ],
+                    options: {
+                        series: {
+                            lines: {show: true},
+                            points: {show: false}
+                        }
+                    }
+                };
             }
 
         }
-	}
+    }
 
-	self.compute = function(state) {
-		return self.dna(state);
-	}
+    self.compute = function (state) {
+        return self.dna(state);
+    }
 }
