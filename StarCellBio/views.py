a668fa28e7a91da2572349a16e3e00e98b208775
Ivica Ceraj <ivan@bugaco.com>
2013-03-21 10:58:54 -0400

    Tufts intructions

--- StarCellBio/views.py
+++ StarCellBio/views.py
@@ -6,6 +6,17 @@ from django.http import HttpResponse
 import datetime
 
 from django.shortcuts import redirect
+import json
+import pudb
 
 def home(request):
     return redirect( 'static/index.html')
+
+def is_auth(request):
+    if request.user.is_authenticated():
+    # Do something for authenticated users.
+        pudb.set_trace()
+        return HttpResponse(json.dumps({ 'user':request.user.username}))
+    else:
+        return HttpResponse(json.dumps({ 'user':None}))
+    # Do something for anonymous users.
--- html_app/assignments/tufts/gen/instructions.soy.js
+++ html_app/assignments/tufts/gen/instructions.soy.js
@@ -4,6 +4,20 @@
 if (typeof scb_assignment_specific_tufts == 'undefined') { var scb_assignment_specific_tufts = {}; }
 
 
+scb_assignment_specific_tufts.assignment_overview = function(opt_data, opt_sb) {
+  var output = opt_sb || new soy.StringBuilder();
+  output.append('<div class=\'scb_s_tufts_assignment_overview\'>For this homework, you will use the new program StarCellBio, which is a cell and molecular biology experiment simulator. Use the program to answer the following questions. This assignment will review material from previous labs and help you prepare for interpreting your final blots on April 11th.</div>');
+  return opt_sb ? '' : output.toString();
+};
+
+
+scb_assignment_specific_tufts.assignment_detail = function(opt_data, opt_sb) {
+  var output = opt_sb || new soy.StringBuilder();
+  output.append('<div class=\'scb_s_tufts_assignment_detail\'>Please use the program to design and interpret two experiments:<br>(1) Use the program to design an experiment to determine if the EGFR kinase inhibitor (erlotinib) blocks activation of the Ras pathway downstream of the EGFR. Print a copy of the final exposed blot(s), which should include a list of the samples you chose. Note: Not all samples should be used in this experiment. Use the samples that will directly answer the question asked. Make sure to include a loading control.<br>(2) Use the program to design an experiment to determine if the Mek kinase inhibitor (U0126) blocks activation of the Ras pathway downstream of the EGFR. Print a copy of the final exposed blot(s), which should include a list of the samples you chose. Note: Not all samples should be used in this experiment. Use the samples that will directly answer the question asked. Make sure to include a loading control.<br></div>');
+  return opt_sb ? '' : output.toString();
+};
+
+
 scb_assignment_specific_tufts.experiment_setup = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
   output.append('<ul class=\'scb_assignment_specific_tufts_experiment_setup\'><li>To setup your experiment, select <b>Add Multiple Rows</b> in the experimental set-up table below. <br></li><li>Select all of the treatment protocols for your experiment and then click <b>Add Multiple Treatments</b>.</li></ul>');
