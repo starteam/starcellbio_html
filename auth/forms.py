3a33f58ad7f57482ff01e4dfa8710eef657a1706
skini <skini@mit.edu>
2013-09-17 10:40:03 -0400

    finished! now the auth.forms file factors out the code correctly and creates usercourse correctly

--- auth/forms.py
+++ auth/forms.py
@@ -2,26 +2,20 @@ from __future__ import absolute_import
 
 from django import forms
 
-from allauth.account.forms import BaseSignupForm
-from allauth.account.utils import (user_username, user_email,
-                                   user_field)
-
-from .models import SocialAccount
-from .adapter import get_adapter
-from . import app_settings
-from . import signals
-
-
-class SignupForm(BaseSignupForm):
-	import pudb
-	pudb.set_trace()
-
-    def save(self, request):
-        super(SignupForm, self).save(self,request)
-        return new_user
-    
-    def after_signup(self, user, **kwargs):
-    	import pudb
-    	pudb.set_trace()
-    	print 'apple'
-    	super(SignupForm, self).after_signup()
+class SignupForm(forms.Form):
+	def save(self, request):
+		import sys
+		import os
+		try:
+			import course
+		except ImportError:
+			sys.path.append(os.path.dirname('/Users/admin/Desktop/VE/scb2/starcellbio_html/auth/course.py'))
+			try:
+				import course
+			finally:
+				sys.path.remove(os.path.dirname('/Users/admin/Desktop/VE/scb2/starcellbio_html/auth/course.py'))
+		course.create_course_records(request,self.data.get('CC'))
+        """
+        An extension point for subclasses.
+        """
+        pass
