51ea53fbebeb313772142a9620879aa8075b7f8b
skini <skini@mit.edu>
2013-12-03 15:08:10 -0500

    fix messages

--- html_app/ui/MainFrame.js
+++ html_app/ui/MainFrame.js
@@ -467,17 +467,11 @@ scb.ui.MainFrame = function scb_ui_MainFrame(master_model, context) {
 					});
 				});
 				
-				iframe.find(".primaryAction").click(function(){
+				iframe.find(".login_submit").click(function(){
 						   $('.iframe').load(function(){
 						   	  var profile = $('.iframe').contents();
 						   	  //console.log(profile);
 						   	  if(profile[0].body.textContent.indexOf('confirmed') >0){
-						   	  	  window.onload = function(){
-						   	  	  	
-						   	  	  	//$('body').css('overflow', 'hidden');
-						   	  	  	//$.jqDialog.alert("Please check your email to confirm this account.", function() {	$('body').css('overflow', 'visible');/* callback function for 'OK' button*/ });
-
-						   	  	  }
 						   	  	  parent.document.location.reload();
 
 							   	  }
