e94f214e2a1d49ca1e07b9f109dfbc5f0bbc3c15
skini <skini@mit.edu>
2014-02-28 09:58:01 -0500

    working i icons for the microscopy section

--- html_app/ui/MicroscopyView.js
+++ html_app/ui/MicroscopyView.js
@@ -1650,6 +1650,22 @@ scb.ui.static.MicroscopyView.register = function (workarea) {
 			}
 		}    
 		});
+		
+	scb.utils.off_on(workarea, 'mouseup', document, function(e,ui){
+    	var container = $(".scb_f_controls_note");
+		container.slideUp(); // hide
+    });
+    scb.utils.off_on(workarea, 'click','.scb_f_controls_note', function(e,ui){
+    	e.stopPropagation();
+    });
+    scb.utils.off_on(workarea, 'click','.scb_f_info_icon', function(e,ui){
+    	e.stopPropagation();
+    	var note = $(this).attr('note');
+    	note = '.' +note;
+    	if($(note).is(":visible"))
+    		$(note).slideUp();
+    	else $(note).slideDown();
+    });
 
 }
 
