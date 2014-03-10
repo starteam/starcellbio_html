0e41dbe58490f6c255ee92d181d620fd560889eb
skini <skini@mit.edu>
2014-03-10 17:02:40 -0400

    exploring alternative for brightness and blur

--- html_app/ui/MicroscopyView.js
+++ html_app/ui/MicroscopyView.js
@@ -858,7 +858,9 @@ function draw(state){
 
 	console.log('draw');
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////
+	
+///////////////////////////////////////////////////////////////////////////////////////////
 
 
 function full_modify_cache(state){
@@ -975,6 +977,7 @@ function init(state, isNew, isIF, draw, image_source){
 			}
 			ctx.drawImage(img, 0, 0, img_width, img_height);	
 			var img2string=canvas.toDataURL(0,0, img.width, img.height);
+			//var img2data = ca
 			canvas.width = scb.ui.static.MicroscopyView.LENS;
 			canvas.height = scb.ui.static.MicroscopyView.LENS;
 			if(isNew){
@@ -1007,8 +1010,108 @@ function init(state, isNew, isIF, draw, image_source){
 				state.cache = img2string;
 			}
 			state.action = 'initialized';
-			
-			save_and_draw_cache_image(canvas,state);
+			//ctx.putImageData(Filters.getPixels(img), state.xparam, state.yparam);
+			var Filters = {};
+			Filters.getPixels = function() { 
+		
+			  var c = this.getCanvas(img.width, img.height);
+			  var ctx = c.getContext('2d');
+			  ctx.fillRect(0,0, c.width, c.height);
+
+				ctx.save();
+				ctx.drawImage(img,0,0);
+				return ctx.getImageData(0,0,c.width,c.height);
+	  
+			};
+			Filters.getCanvas = function(w,h) {
+			  var c = document.createElement('canvas');
+			  c.id = 'lens';
+			  c.width = w;
+			  c.height = h; 
+			  //$('.head')[0].appendChild(c);
+			  return c;
+			};
+			Filters.filterImage = function(filter, image, var_args) {
+			  var args = [this.getPixels()];
+			  for (var i=2; i<arguments.length; i++) {
+				args.push(arguments[i]);
+			  }
+			  return filter.apply(null, args);
+			};
+			Filters.brightness = function(pixels, adjustment) {
+			  var d = pixels.data;
+			  for (var i=0; i<d.length; i+=4) {
+				d[i] += adjustment;
+				d[i+1] += adjustment;
+				d[i+2] += adjustment;
+			  }
+			  return pixels;
+			};
+			Filters.tmpCanvas = document.createElement('canvas');
+			Filters.tmpCtx = Filters.tmpCanvas.getContext('2d');
+			Filters.createImageData = function(w,h) {
+			  return this.tmpCtx.createImageData(w,h);
+			};
+			Filters.convolute = function(pixels, weights, opaque) {
+			  var side = Math.round(Math.sqrt(weights.length));
+			  var halfSide = Math.floor(side/2);
+			  var src = pixels.data;
+			  var sw = pixels.width;
+			  var sh = pixels.height;
+			  // pad output by the convolution matrix
+			  var w = sw;
+			  var h = sh;
+			  var output = Filters.createImageData(w, h);
+			  var dst = output.data;
+			  // go through the destination image pixels
+			  var alphaFac = opaque ? 1 : 0;
+			  for (var y=0; y<h; y++) {
+				for (var x=0; x<w; x++) {
+				  var sy = y;
+				  var sx = x;
+				  var dstOff = (y*w+x)*4;
+				  // calculate the weighed sum of the source image pixels that
+				  // fall under the convolution matrix
+				  var r=0, g=0, b=0, a=0;
+				  for (var cy=0; cy<side; cy++) {
+					for (var cx=0; cx<side; cx++) {
+					  var scy = sy + cy - halfSide;
+					  var scx = sx + cx - halfSide;
+					  if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
+						var srcOff = (scy*sw+scx)*4;
+						var wt = weights[cy*side+cx];
+						r += src[srcOff] * wt;
+						g += src[srcOff+1] * wt;
+						b += src[srcOff+2] * wt;
+						a += src[srcOff+3] * wt;
+					  }
+					}
+				  }
+				  dst[dstOff] = r;
+				  dst[dstOff+1] = g;
+				  dst[dstOff+2] = b;
+				  dst[dstOff+3] = a + alphaFac*(255-a);
+				}
+			  }
+			  return output;
+			};
+	
+		function generateFilter(number){
+		var filter = [];
+		var square = number * number;
+		for(var x = 0; x<number; x++){
+			for(var y = 0; y<number; y++){
+				filter.push(1/square);
+			}
+		}
+		
+		console.log(filter.length);
+		return filter;
+		
+	};
+			var grayscale = Filters.filterImage(Filters.convolute, img,generateFilter(1));
+			ctx.putImageData(grayscale, state.xparam, state.yparam );
+			//save_and_draw_cache_image(canvas,state);
 
 			draw(state);
 
