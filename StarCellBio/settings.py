1773519af824cb57dd8772a6b8a6ed3910ea0e54
Ivica Ceraj <iceraj@gmail.com>
2014-09-10 16:55:22 -0400

    Remove extra files

--- StarCellBio/settings.py
+++ StarCellBio/settings.py
@@ -142,7 +142,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
      'django.contrib.admindocs',
-     'frontend_tests',
      'rest_framework',
      'backend',
      'instructor',
