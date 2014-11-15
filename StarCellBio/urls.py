9754647d0c1acb39b5cc7560b5e2a442fc103409
Ivica Ceraj <iceraj@gmail.com>
2014-11-15 15:48:32 -0500

    Courses editor

--- StarCellBio/urls.py
+++ StarCellBio/urls.py
@@ -4,25 +4,30 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from StarCellBio import settings
 import auth.urls
+from backend import courseview
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        # Examples:
                        url(r'^$', "StarCellBio.views.home", name='home'),
-#                        url(r'^instructor/', "StarCellBio.views.home", name='home'),
+                       # url(r'^instructor/', "StarCellBio.views.home", name='home'),
                        url(r'^index.html$', "StarCellBio.views.home"),
                        url(r'^scb/contact$', "StarCellBio.views.contact", name='contact'),
                        url(r'^scb/get_model.js$', "StarCellBio.views.get_model", name='get_model'),
-                       url(r'^scb/initialize_courses.js$', "StarCellBio.views.initialize_courses", name='initialize_courses'),
-                       url(r'^scb/get_student_courses.js$', "StarCellBio.views.get_student_courses", name='get_student_courses'),
-                       url(r'^scb/get_instructor_assignments.js$', "StarCellBio.views.get_instructor_assignments", name='get_instructor_assignments'),
+                       url(r'^scb/initialize_courses.js$', "StarCellBio.views.initialize_courses",
+                           name='initialize_courses'),
+                       url(r'^scb/get_student_courses.js$', "StarCellBio.views.get_student_courses",
+                           name='get_student_courses'),
+                       url(r'^scb/get_instructor_assignments.js$', "StarCellBio.views.get_instructor_assignments",
+                           name='get_instructor_assignments'),
                        url(r'^scb/edit_assignment.js$', "StarCellBio.views.edit_assignment", name='edit_assignment'),
 
                        url(r'^scb/create_course.js$', "StarCellBio.views.create_course", name='create_course'),
-                       url(r'^scb/create_new_assignment.js$', "StarCellBio.views.create_new_assignment", name='create_new_assignment'),
+                       url(r'^scb/create_new_assignment.js$', "StarCellBio.views.create_new_assignment",
+                           name='create_new_assignment'),
 
-                       
+
                        url(r'^scb/get_user.js$', "StarCellBio.views.get_user", name='get_user'),
                        url(r'^scb/post_state.js$', "StarCellBio.views.post_state", name='post_state'),
                        # Uncomment the admin/doc line below to enable admin documentation:
@@ -34,11 +39,18 @@ urlpatterns = patterns('',
                            {'document_root': 'html_app/images/'}),
                        url(r'^pdf/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'html_app/pdf/'}),
                        url(r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'html_app/js/'}),
-                       
+
 )
 # add authentication URL patterns
 urlpatterns += auth.urls.urlpatterns
+#urlpatterns += courseview.urlpatterns
 
+urlpatterns += patterns('',
+                        url(r'^courses/$', 'backend.courseview.list', name='course_list'),
+                        url(r'^courses/new$', 'backend.courseview.create', name='course_new'),
+                        url(r'^courses/edit/(?P<pk>.+)$', 'backend.courseview.update', name='course_edit'),
+                        url(r'^courses/delete/(?P<pk>.+)$', 'backend.courseview.delete', name='course_delete'),
+)
 from tastypie.api import Api
 from backend.services import UserResource, CourseResource, AssignmentResource, StudentAssignmentResource
 
