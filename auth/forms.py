f5c82c216c1c2f2c4d7416662ab41d9f276eb3aa
annagav <annagav@mit.edu>
2015-04-22 16:56:46 -0400

    auth forms.py code reformat

--- auth/forms.py
+++ auth/forms.py
@@ -2,51 +2,29 @@ from __future__ import absolute_import
 
 from django import forms
 
-from backend.models import UserCourse, Course, Assignment
-from django.contrib.auth.models import User, Group
+from backend.models import UserCourse, Course
+from django.contrib.auth.models import Group
 
-import StarCellBio.settings as settings
 
 class SignupForm(forms.Form):
-	def save(self, request):
-		import sys
-		import os
-		import pudb
-# 		pudb.set_trace()
-		if(self.data.get('account_type') == 'student'):
-			create_course_records(request,self.data.get('CC'))
-# 		elif(self.data.get('account_type') == 'instructor'):
-		add_to_group(request, self.data.get('account_type'))
-        """
-        An extension point for subclasses.
-        """
-        pass
+
+    def save(self, request):
+        if self.data.get('account_type') == 'student':
+            create_course_records(request, self.data.get('CC'))
+        add_to_group(request, self.data.get('account_type'))
 
 
+def create_course_records(usr, cc_input):
+    cc_input = cc_input.replace(' ', '')
+    course_codes = cc_input.split(';')
+    for course_code in course_codes:
+        if Course.objects.filter(code=course_code).count() > 0:
+            course = Course.objects.get(code=course_code)
+            user_course = UserCourse(course_name=course_code, user=usr, courseID=course)
+            user_course.save()
 
 
-
-
-
-
-def create_course_records(usr, input):
-	import pudb
-	#pudb.set_trace()
-	input = input.replace(' ', '')
-	course_codes = input.split(';')
-	for course_code in course_codes:
-		if(Course.objects.filter(code = course_code).count()>0):
-			course = Course.objects.get(code = course_code)
-			user_course = UserCourse(course_name=course_code, user=usr, courseID = course)
-			user_course.save()
-			print course_code
-
-	# get the data
-	
 def add_to_group(usr, input):
-	import pudb
-	#pudb.set_trace()
-	g = Group.objects.filter(name=input)
-	usr.groups.add(g[0])
-	usr.save()
-	
+    g = Group.objects.filter(name=input)
+    usr.groups.add(g[0])
+    usr.save()
